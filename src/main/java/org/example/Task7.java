0708653a0f67053f366780a745af127f55942190
Roman <92824863+lee-roman@users.noreply.github.com>
2024-06-29 20:39:22 +0300

    Lesson2

--- src/main/java/org/example/Task7.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-// Вызвать через sout в главном методе
-//System.out.println(Task7.isNegative(0));
-public class Task7 {
-    public static boolean isNegative(int number) {
-        return number < 0;
-    }
-
-}
--- src/main/java/org/example/Task9.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-// Вызвать через sout в главном методе
-//System.out.println(Task9.isLeapYear(2024));
-public class Task9 {
-    public static boolean isLeapYear(int year) {
-        return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
-    }
-
-}
