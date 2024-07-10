c5d29b275f7324cc622f9e90cd78642d68b62f37
Roman <92824863+lee-roman@users.noreply.github.com>
2024-07-10 19:46:53 +0300

    Практическое задание 10

--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        FactorialCalculator calculator = new FactorialCalculator();
-        long result = calculator.calculateFactorial(5);
-        System.out.println("5! = " + result);
-    }
-}
