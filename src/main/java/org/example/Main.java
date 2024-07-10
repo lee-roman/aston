4f68b80554f412b495115499b696823e25e5082d
Roman <92824863+lee-roman@users.noreply.github.com>
2024-07-10 19:45:14 +0300

    Практическое задание 10

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,9 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введите число для вычисления факториала: ");
-        int number = scanner.nextInt();
-
-        try {
-            long factorial = FactorialCalculator.calculateFactorial(number);
-            System.out.println("Факториал числа " + number + " равен " + factorial);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        scanner.close();
+        FactorialCalculator calculator = new FactorialCalculator();
+        long result = calculator.calculateFactorial(5);
+        System.out.println("5! = " + result);
     }
 }
